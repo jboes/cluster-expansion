ab6317691ce4624596a667370ee5660a619628bb
Jacob Boes <jboes@andrew.cmu.edu>
2015-12-11 16:53:41 -0500

    completed energy ONLY neural

---
+++ networks/db2/40-12-12-1/submit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from amp import Amp
+from amp.descriptor import Behler
+from amp.regression import NeuralNetwork
+
+calc = Amp(label="./",
+           dblabel="../other-",
+           descriptor=Behler(cutoff=6.0),
+           regression=NeuralNetwork(hiddenlayers=(2, 12)))
+
+calc.train("../train.db",
+           cores=12,
+           force_goal=None,
+           extend_variables=False)
---
+++ networks/db2/40-14-14-1/submit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from amp import Amp
+from amp.descriptor import Behler
+from amp.regression import NeuralNetwork
+
+calc = Amp(label="./",
+           descriptor=Behler(cutoff=6.0),
+           regression=NeuralNetwork(hiddenlayers=(2, 14)))
+
+calc.train("../train.db",
+           cores=12,
+           force_goal=None,
+           extend_variables=False)
