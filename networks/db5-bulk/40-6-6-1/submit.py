3babe4e97f65de45cc045b3bed2357073a61bf6d
Jacob Boes <jboes@andrew.cmu.edu>
2016-03-09 13:30:19 -0500

    addition of db5-bulk calculations

---
+++ networks/db5-bulk/40-6-6-1/submit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from amp import Amp
+from amp.descriptor import Behler
+from amp.regression import NeuralNetwork
+
+calc = Amp(label="./",
+           descriptor=Behler(cutoff=6.5),
+           regression=NeuralNetwork(hiddenlayers=(2, 6)))
+
+calc.train("../train.db",
+           cores=16,
+           energy_goal=0.0005,
+           force_goal=None,
+           extend_variables=False)
